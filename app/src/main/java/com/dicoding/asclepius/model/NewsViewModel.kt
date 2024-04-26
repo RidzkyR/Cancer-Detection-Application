1b9187ae64a3dd1586fd0b76246f87a94f42b5b3
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-26 22:00:02 +0700

    move showNews to ViewModel

--- app/src/main/java/com/dicoding/asclepius/model/NewsViewModel.kt
+++ app/src/main/java/com/dicoding/asclepius/model/NewsViewModel.kt
@@ -1,17 +1,52 @@
 package com.dicoding.asclepius.model
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dicoding.asclepius.data.response.ArticlesItem
+import com.dicoding.asclepius.data.response.NewsResponse
+import com.dicoding.asclepius.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NewsViewModel : ViewModel() {
-    private val _restaurant = MutableLiveData<ArticlesItem>()
-    val restaurant: LiveData<ArticlesItem> = _restaurant
-
-    private val _listReview = MutableLiveData<List<ArticlesItem>>()
-    val listReview: LiveData<List<ArticlesItem>> = _listReview
+    private val _listNews = MutableLiveData<List<ArticlesItem>>()
+    val listNews: MutableLiveData<List<ArticlesItem>> = _listNews
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        showNews()
+    }
+
+    private fun showNews() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getNews(QUERY, API_KEY)
+        client.enqueue(object : Callback<NewsResponse> {
+            override fun onResponse(
+                call: Call<NewsResponse>,
+                response: Response<NewsResponse>,
+            ) {
+                _isLoading.value = false
+                val responseBody = response.body()
+                if (response.isSuccessful && response.body() != null) {
+                    _listNews.value = (responseBody?.articles?.filterNot { it.title == "[Removed]" })
+                }
+            }
+
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
+    }
+
+    companion object {
+        private const val TAG = "NewsActivity"
+        private const val QUERY = "cancer"
+        private const val API_KEY = "b4ab6900c9ff4e139a4678cddb5ad82e"
+    }
 }
--- app/src/main/java/com/dicoding/asclepius/view/ui/NewsActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/ui/NewsActivity.kt
@@ -6,17 +6,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.data.response.ArticlesItem
-import com.dicoding.asclepius.data.response.NewsResponse
-import com.dicoding.asclepius.data.retrofit.ApiConfig
 import com.dicoding.asclepius.databinding.ActivityNewsBinding
+import com.dicoding.asclepius.model.NewsViewModel
 import com.dicoding.asclepius.view.adapter.NewsAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
@@ -38,28 +35,16 @@ class NewsActivity : AppCompatActivity() {
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvNews.addItemDecoration(itemDecoration)
 
-        showNews()
-    }
+        //inisialisasi NewsViewModel
+        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(NewsViewModel::class.java)
 
-    private fun showNews() {
-        showLoading(true)
-        val client = ApiConfig.getApiService().getNews(QUERY, API_KEY)
-        client.enqueue(object : Callback<NewsResponse> {
-            override fun onResponse(
-                call: Call<NewsResponse>,
-                response: Response<NewsResponse>
-            ) {
-                showLoading(false)
-                val responseBody = response.body()
-                if (response.isSuccessful && response.body() != null) {
-                    setNewsData(responseBody?.articles?.filterNot { it.title == "[Removed]" })
-                }
-            }
+        mainViewModel.listNews.observe(this) {
+            setNewsData(it)
+        }
 
-            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                showLoading(false)
-            }
-        })
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
     }
 
     private fun setNewsData(newsData: List<ArticlesItem>?) {
@@ -74,10 +59,5 @@ class NewsActivity : AppCompatActivity() {
         } else {
             binding.progressBar.visibility = View.GONE
         }
-    }
-
-    companion object{
-        private const val QUERY = "cancer"
-        private const val API_KEY = "b4ab6900c9ff4e139a4678cddb5ad82e"
     }
 }
